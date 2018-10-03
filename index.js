9fce6f649360ccefb7edd889f3b74c6919771d7f
horita.yuichi <horita.yuichi@infogram.co.jp>
2018-10-03 15:03:51 +0900

    improvement

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-var Logger = (function(){
-    var Logger = function(){
+var AudioContext = window.AudioContext || window.webkitAudioContext;
+
+var Logger = (function () {
+    var Logger = function () {
     };
 
-    Logger.prototype.log = function(msg){
+    Logger.prototype.log = function (msg) {
         console.log(msg);
         document.getElementById("log").innerHTML += msg + "\n";
     };
@@ -10,40 +12,75 @@ var Logger = (function(){
     return Logger;
 })();
 
-var Listener = (function(){
+var Listener = (function () {
 
-    var Listener = function(speechToText,stateChanged,logger){
+    var Listener = function (speechToText, stateChanged, logger) {
         var that = this;
         this.state = Listener.INITIALIZING;
         this.speechToText = speechToText;
         this.stateChanged = stateChanged;
         this.logger = logger;
 
-        navigator.mediaDevices.getUserMedia({ audio: true, video: false }).then(function(stream){
+        navigator.mediaDevices.getUserMedia({ audio: true, video: false }).then(function (stream) {
+            that.detectSilence(stream);
             that.onGetUserMediaComplete(stream);
         }).catch(function (reason) {
             that.onError(reason);
         });
     };
-    
+
     Listener.INITIALIZING = 0;
     Listener.INITIALIZED = 1;
     Listener.LISTENING = 2;
 
     Listener.prototype.convertoFloat32ToInt16 = function (buffer) {
         var l = buffer.length;
-        var buf = new Int16Array(l/2);
+        var buf = new Int16Array(l / 2);
         while (l--) {
-            if(l%2==0){
-                buf[l/2] = buffer[l] * 0xFFFF;
+            if (l % 2 == 0) {
+                buf[l / 2] = buffer[l] * 0xFFFF;
             }
         }
         return buf.buffer
     };
 
+    Listener.prototype.detectSilence = function (
+        stream,
+        silence_delay = 5000,
+        min_decibels = -80
+    ) {
+        var that = this;
+        const ctx = new AudioContext();
+        const analyser = ctx.createAnalyser();
+        const streamNode = ctx.createMediaStreamSource(stream);
+        streamNode.connect(analyser);
+        analyser.minDecibels = min_decibels;
+
+        const data = new Uint8Array(analyser.frequencyBinCount); // will hold our data
+        let silence_start = performance.now();
+        let triggered = false; // trigger only once per silence event
+
+        function loop(time) {
+            requestAnimationFrame(loop); // we'll loop every 60th of a second to check
+            analyser.getByteFrequencyData(data); // get current data
+            if (data.some(v => v)) { // if there is data above the given db limit
+                if (that.state === Listener.INITIALIZED) {
+                    that.logger.log("Speaking detected.");
+                    that.start();
+                }
+                silence_start = time; // set it to now
+            }
+            if (that.state == Listener.LISTENING && time - silence_start > silence_delay) {
+                that.logger.log("Silence detected.");
+                that.stop();
+            }
+        }
+        loop();
+    };
+
     Listener.prototype.onGetUserMediaComplete = function (stream) {
         var that = this;
-        var AudioContext = window.AudioContext || window.webkitAudioContext;
+
         var context = new AudioContext();
         var input = context.createMediaStreamSource(stream)
         var processor = context.createScriptProcessor(1024, 1, 1);
@@ -61,35 +98,43 @@ var Listener = (function(){
         that.setState(Listener.INITIALIZED);
     };
 
-    Listener.prototype.setState = function(val){
-        if(this.state !== val){
+    Listener.prototype.setState = function (val) {
+        if (this.state !== val) {
             this.state = val;
             this.onStateChanged();
         }
     };
 
-    Listener.prototype.start = function(){
-        if(this.state === Listener.INITIALIZED){
+    Listener.prototype.start = function () {
+        if (this.state === Listener.INITIALIZED) {
             this.setState(Listener.LISTENING);
         } else {
             this.onError("Listener is not initialized.");
         }
     };
 
-    Listener.prototype.stop = function(){
-        if(this.state === Listener.LISTENING){
+    Listener.prototype.stop = function () {
+        if (this.state === Listener.LISTENING) {
             this.speechToText.stop();
             this.setState(Listener.INITIALIZED);
         }
     };
 
-    Listener.prototype.onStateChanged = function(){
-        if(this.stateChanged){
-            this.stateChanged();
+    Listener.prototype.toggle = function(){
+        if(this.state === Listener.LISTENING){
+            this.stop();
+        } else if(this.state === Listener.INITIALIZED){
+            this.start();
+        }
+    }
+
+    Listener.prototype.onStateChanged = function () {
+        if (this.stateChanged) {
+            this.stateChanged(this.state);
         }
     };
 
-    Listener.prototype.onError = function(msg){
+    Listener.prototype.onError = function (msg) {
         this.logger.log(msg);
     };
 
@@ -97,7 +142,7 @@ var Listener = (function(){
 })();
 
 var SpeechToText = (function () {
-    var SpeechToText = function (token,logger) {
+    var SpeechToText = function (token, logger) {
         this.wsURI = "wss://stream.watsonplatform.net/speech-to-text/api/v1/recognize"
             + "?watson-token=" + token
             + "&model=ja-JP_BroadbandModel";
@@ -106,72 +151,74 @@ var SpeechToText = (function () {
         this.isopening = false;
     };
 
-    SpeechToText.prototype.open = function(blob){
-        if(!this.isopening){
+    SpeechToText.prototype.open = function (blob) {
+        if (!this.isopening) {
             this.close();
             this.isopening = true;
             var that = this;
             this.websocket = new WebSocket(this.wsURI);
-            this.websocket.onopen = function(){
+            this.websocket.onopen = function () {
                 that.start();
-                if(blob){
-                    that.send(blob);
+                if (blob) {
+                    that.send(blob, false);
                 }
                 that.isopening = false;
                 that.logger.log("WebSocket connection is opened.");
             };
-            this.websocket.onmessage = function(evt){
+            this.websocket.onmessage = function (evt) {
                 that.onMessage(evt);
             };
-            this.websocket.onerror = function(evt){
+            this.websocket.onerror = function (evt) {
                 that.onError("WebSocket connection error.");
             };
-            this.websocket.onclose = function(evt){
-                that.onClose("WebSocket connection is closed.");
+            this.websocket.onclose = function (evt) {
+                that.onClose("WebSocket connection is closed. [" + evt.reason + "]");
             };
         }
     };
 
-    SpeechToText.prototype.close = function(){
-        if(this.websocket && this.websocket.readyState === 1) {
+    SpeechToText.prototype.close = function () {
+        if (this.websocket && this.websocket.readyState === 1) {
             this.websocket.close();
         }
         this.websocket = null;
         this.isopening = false;
     }
 
-    SpeechToText.prototype.start = function(){
-        this.websocket.send(JSON.stringify({
-            "action": "start",
-            "content-type": "audio/l16;rate=22050"
-        }));
+    SpeechToText.prototype.start = function () {
+        if (this.websocket && this.websocket.readyState === 1) {
+            this.websocket.send(JSON.stringify({
+                "action": "start",
+                "content-type": "audio/l16;rate=22050"
+            }));
+        }
     };
 
-    SpeechToText.prototype.send = function (blob) {
+    SpeechToText.prototype.send = function (blob, auto = true) {
         var that = this;
-        if(this.websocket && this.websocket.readyState === 1) {
+        if (this.websocket && this.websocket.readyState === 1) {
             this.websocket.send(blob);
-        } else {
+        } else if (auto) {
             this.open(blob);
         }
     };
 
-    SpeechToText.prototype.stop = function(){
-        if(this.websocket && this.websocket.readyState === 1) {
+    SpeechToText.prototype.stop = function () {
+        if (this.websocket && this.websocket.readyState === 1) {
             this.websocket.send(JSON.stringify({ "action": "stop" }));
         }
     };
 
-    SpeechToText.prototype.onMessage = function(evt) {
+    SpeechToText.prototype.onMessage = function (evt) {
         this.logger.log(evt.data);
     };
 
-    SpeechToText.prototype.onError = function(msg) {
+    SpeechToText.prototype.onError = function (msg) {
         this.logger.log(msg);
         this.close();
     };
 
-    SpeechToText.prototype.onClose = function(msg) {
+    SpeechToText.prototype.onClose = function (msg) {
         this.logger.log(msg);
         this.close();
     };
@@ -179,25 +226,34 @@ var SpeechToText = (function () {
     return SpeechToText;
 })();
 
+document.getElementById("step2").style.display = "none";
+
 var logger = new Logger();
 var listener = null;
 
 document.getElementById("open").addEventListener("click", function () {
     var key = document.getElementById("token").value;
-    var stt = new SpeechToText(key,logger);
-    listener = new Listener(stt,function(){},logger);
+    var stt = new SpeechToText(key, logger);
+    listener = new Listener(stt, function (state) {
+        if(state === Listener.INITIALIZED){
+            document.getElementById("toggle").innerHTML = "START";
+            document.getElementById("toggle").classList.add("btn-success");
+            document.getElementById("toggle").classList.remove("btn-danger");
+        } else if(state === Listener.LISTENING){
+            document.getElementById("toggle").innerHTML = "STOP";
+            document.getElementById("toggle").classList.remove("btn-success");
+            document.getElementById("toggle").classList.add("btn-danger");
+        }
+    }, logger);
+
+    document.getElementById("step1").style.display = "none";
+    document.getElementById("step2").style.display = "block";
 });
 
 document.getElementById("toggle").addEventListener("click", function () {
-    try{
-        if (listener.state === Listener.LISTENING) {
-            listener.stop();
-            document.getElementById("toggle").innerHTML = "START";
-        } else if(listener.state === Listener.INITIALIZED){
-            listener.start();
-            document.getElementById("toggle").innerHTML = "STOP";
-        }
-    } catch (e){
+    try {
+        listener.toggle();
+    } catch (e) {
         logger.log(e);
     }
 });
