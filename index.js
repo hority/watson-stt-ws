468cfdfb5c098eaa04f2e8f8649d45b72bab1cfa
horita.yuichi <horita.yuichi@infogram.co.jp>
2018-10-05 10:14:47 +0900

    raise min decibels

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ var Listener = (function () {
     Listener.prototype.detectSilence = function (
         stream,
         silence_delay = 5000,
-        min_decibels = -80
+        min_decibels = 50
     ) {
         var that = this;
         const ctx = new AudioContext();
